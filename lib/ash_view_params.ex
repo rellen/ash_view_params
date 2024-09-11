5c7f6902704407084cb532ec1daef76012bb6c1e
Robert Ellen <rellen@ieee.org>
2024-09-11 23:32:33 +1000

    chore: add skeleton app

---
+++ lib/ash_view_params.ex
@@ -0,0 +1,18 @@
+defmodule AshViewParams do
+  @moduledoc """
+  Documentation for `AshViewParams`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshViewParams.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_view_params_test.exs
@@ -0,0 +1,8 @@
+defmodule AshViewParamsTest do
+  use ExUnit.Case
+  doctest AshViewParams
+
+  test "greets the world" do
+    assert AshViewParams.hello() == :world
+  end
+end
